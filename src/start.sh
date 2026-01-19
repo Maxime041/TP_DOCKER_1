cbffd662d15cdab5e876c8406f813490f03fbe6b
Maxime04 <maxime.danino04@gmail.com>
2026-01-19 17:02:29 +0100

    Ajout de MariaDB et installation de phpMyAdmin

--- src/start.sh
+++ src/start.sh
@@ -2,10 +2,20 @@
 
 echo "ServerName localhost" >> /etc/apache2/apache2.conf
 
+service mariadb start
+
+sleep 2
+echo "Création de la base et de l'utilisateur..."
+echo "CREATE DATABASE wordpress;" | mariadb -u root
+echo "CREATE USER 'maximetest'@'localhost' IDENTIFIED BY 'secret';" | mariadb -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO 'maximetest'@'localhost';" | mariadb -u root
+echo "FLUSH PRIVILEGES;" | mariadb -u root
+
 if [ "$INDEX" = "1" ]; then
     echo "Mode INDEX=1 ACTIVÉ"
 
     rm -f /var/www/html/index.php
+    rm -rf /var/www/html/phpmyadmin
 
     # On modifie la configuration d'Apache pour activer les Indexes
     sed -i "s/Indexes//g" /etc/apache2/apache2.conf
